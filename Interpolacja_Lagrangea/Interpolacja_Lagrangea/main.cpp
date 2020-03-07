545313af5d767a272275a348d85de275fd302825
MatWich <wicherskimateusz@gmail.com>
2020-03-07 22:36:41 +0100

    Dodano interpolacje metoda Newtona

--- Interpolacja_Lagrangea/Interpolacja_Lagrangea/main.cpp
+++ Interpolacja_Lagrangea/Interpolacja_Lagrangea/main.cpp
@@ -10,6 +10,7 @@
 void IntLagr();
 void menu();
 void decyzja_menu(char);
+void Int_Newtona();
 
 int main()
 {
@@ -89,7 +90,84 @@ void IntLagr()
     std::cout << "Nacisnij enter by kontynuowac"; 
     _getch();
     
+	delete[] xtab;
+	delete[] ytab;
     file.close();
+}
+
+void Int_Newtona()
+{
+	int i, j;	//iteratory do petli 
+	std::ifstream file("data.txt");
+
+	if (!file.is_open())
+	{
+		std::cout << "Nie znaleziono pliku sprawdz czy jest w folderze z programem" << std::endl;
+		exit;
+	}
+	else
+	{
+		int ile_wezlow;
+		file >> ile_wezlow;
+
+		double* xtab = new double[ile_wezlow];
+		double* ytab = new double[ile_wezlow];
+
+		std::cout <<"Wczytano: " << ile_wezlow << std::endl;
+
+		for (i = 0; i < ile_wezlow; i++)
+		{
+			file >> xtab[i];
+			file >> ytab[i];
+		}
+
+		//wypisanie
+		for (i = 0; i < ile_wezlow; i++)
+		{
+			std::cout << "x" << i << ": " << xtab[i] << "\t";
+			std::cout << "y" << i << ": " << ytab[i];
+			std::cout << std::endl;
+		}
+
+		double punkt;
+		std::cout << "Podaj punkt: ";
+		std::cin >> punkt;	std::cin.ignore();
+
+		double x = punkt;
+
+		//liczenie wartosci wielomianu w wezlach
+		for (i = 0; i < ile_wezlow - 1; i++)
+		{
+			for (j = ile_wezlow - 1; j > i; j--)
+			{
+				if (i != j)
+				{
+					ytab[j] = (ytab[j] - ytab[j - 1]) / (xtab[j] - xtab[j - i - 1]);
+				}
+			}
+		}
+
+		double sum = 0;	double mult;
+		//liczenie dla podanego x
+		for (i = ile_wezlow - 1; i >= 0; i--)
+		{
+			mult = 1;
+			for (j = 0; j < i; j++)
+			{
+				mult *= (x - xtab[j]);
+			}
+			mult *= ytab[j];
+			sum += mult;
+		}
+		std::cout << "f(" << punkt << ") ~= " << sum << std::endl;
+		
+		std::cout << "Nacisnij enter by kontynuowac";
+		std::cin.get(); //_getch();
+
+		delete[] xtab;
+		delete[] ytab;
+		file.close();
+	}
 }
 
 void menu()
@@ -97,6 +175,7 @@ void menu()
     std::cout << "              MENU " << std::endl;
     std::cout << "################################## " << std::endl;
     std::cout << "1. Interpolacja metoda Lagrange'a " << std::endl;
+	std::cout << "2. Interpolacja metoda Newtona " << std::endl;
     std::cout << "q. Wyjscie z programu " << std::endl;
     std::cout << "################################## " << std::endl;
 }
@@ -108,8 +187,12 @@ void decyzja_menu(char wybor)
     case '1':
         IntLagr();
         break;
+	case '2':
+		Int_Newtona();
+		break;
     default:
         std::cout << "O tym lowco easter eggow ;)" << std::endl;
         break;
     }
 }
+
